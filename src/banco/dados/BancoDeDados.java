c82dfc95b5befb1b1076716297d419f48209e337
kaiokassiano <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2016-05-24 16:02:09 -0300

    Refatoramento na classe de gerencia do banco de dados

--- src/banco/dados/BancoDeDados.java
+++ src/banco/dados/BancoDeDados.java
@@ -1,7 +1,5 @@
 package banco.dados;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,7 +8,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 
 import controller.hospital.HospitalController;
 import model.usuarios.Funcionario;
@@ -22,13 +19,10 @@ import model.usuarios.Funcionario;
 public class BancoDeDados {
 
 	private static BancoDeDados instance = null;
-	
+
 	private final String directory = "system_data";
 
-	// Arquivo e stream representando se o sistema foi liberado
-	private DataOutputStream sistemaLiberadoOutput;
-	private DataInputStream sistemaLiberadoInput;
-	private final String sistemaLiberadoFile = "soos_util1.dat";
+	// Boolean que representa se o sistema esta liberado
 	private boolean sistemaLiberado;
 
 	// Arquivo e stream representando o controller
@@ -36,7 +30,7 @@ public class BancoDeDados {
 	private ObjectInputStream hospitalControllerInput;
 	private final String hospitalControllerFile = "soos.dat";
 	private HospitalController hospitalController;
-	
+
 	// Usuário logado no sistema
 	private Funcionario usuarioLogado;
 
@@ -63,7 +57,7 @@ public class BancoDeDados {
 	 * nos arquivos
 	 */
 	public void init() {
-		//initSistemaLiberado();
+		// initSistemaLiberado();
 		initHospitalController();
 	}
 
@@ -71,26 +65,27 @@ public class BancoDeDados {
 	 * Fecha o banco, salvando todos os dados que estavam na memória em arquivos
 	 */
 	public void fechar() {
-		//fecharSistemaLiberado();
+		// fecharSistemaLiberado();
 		fecharHospitalController();
 	}
 
 	private void initHospitalController() {
 		try {
-			hospitalControllerInput = new ObjectInputStream(new FileInputStream(directory + "/" + hospitalControllerFile));
-			
+			hospitalControllerInput = new ObjectInputStream(
+					new FileInputStream(directory + "/" + hospitalControllerFile));
+
 			try {
 				hospitalController = (HospitalController) hospitalControllerInput.readObject();
 			} catch (ClassNotFoundException | EOFException e) {
 				hospitalController = new HospitalController();
 			}
-			
+
 			hospitalControllerInput.close();
 		} catch (FileNotFoundException e) {
 			hospitalController = new HospitalController();
-			
+
 			File f = new File(directory + "/" + hospitalControllerFile);
-			
+
 			f.getParentFile().mkdirs();
 			try {
 				f.createNewFile();
@@ -101,11 +96,12 @@ public class BancoDeDados {
 			System.err.println("IOException: " + e.getMessage());
 		}
 	}
-	
+
 	private void fecharHospitalController() {
 		try {
-			hospitalControllerOutput = new ObjectOutputStream(new FileOutputStream(directory + "/" + hospitalControllerFile));
-			
+			hospitalControllerOutput = new ObjectOutputStream(
+					new FileOutputStream(directory + "/" + hospitalControllerFile));
+
 			hospitalControllerOutput.writeObject(hospitalController);
 
 			hospitalControllerOutput.close();
@@ -122,53 +118,9 @@ public class BancoDeDados {
 			System.err.println("IOException: " + e.getMessage());
 		}
 	}
-	
+
 	public HospitalController getHospitalController() {
 		return this.hospitalController;
-	}
-	
-	private void initSistemaLiberado() {
-		try {
-			sistemaLiberadoInput = new DataInputStream(new FileInputStream(directory + "/" + sistemaLiberadoFile));
-
-			try {
-				sistemaLiberado = sistemaLiberadoInput.readBoolean();
-			} catch (EOFException e) {
-				sistemaLiberado = false;
-			}
-
-			sistemaLiberadoInput.close();
-		} catch (FileNotFoundException e) {
-			File f = new File(directory + "/" + sistemaLiberadoFile);
-
-			f.getParentFile().mkdirs();
-			try {
-				f.createNewFile();
-			} catch (IOException e1) {
-				System.err.println("IOException: " + e.getMessage());
-			}
-		} catch (IOException e) {
-			System.err.println("IOException: " + e.getMessage());
-		}
-	}
-
-	private void fecharSistemaLiberado() {
-		try {
-			sistemaLiberadoOutput = new DataOutputStream(new FileOutputStream(directory + "/" + sistemaLiberadoFile));
-
-			sistemaLiberadoOutput.writeBoolean(sistemaLiberado);
-		} catch (FileNotFoundException e) {
-			File f = new File(directory + "/" + sistemaLiberadoFile);
-
-			f.getParentFile().mkdirs();
-			try {
-				f.createNewFile();
-			} catch (IOException e1) {
-				System.err.println("IOException: " + e.getMessage());
-			}
-		} catch (IOException e) {
-			System.err.println("IOException: " + e.getMessage());
-		}
 	}
 
 	public boolean isSistemaLiberado() {
